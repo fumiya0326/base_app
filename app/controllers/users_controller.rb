cb3d6e6f83543a2e7fc2bbfe55f699039e538616
fumiya0326 <buchi0326iwa@gmail.com>
2020-03-09 23:17:43 +0900

    fixed after deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,21 @@ class UsersController < ApplicationController
         @user=User.find(params[:id])
         @comment_histories=@user.comment_histories.order(created_at: "DESC")
         @followings=@user.following.all
-        @sules=Sule.where(user_id: @user.id)
+        @sules=Sule.where(user_id: @user.id).order(created_at: "DESC").limit(5)
         @chart=@user.commes.group("date(created_at)").count
         @today_comment=@user.commes.where("created_at LIKE?", "%#{Time.zone.today}%").count+ @user.replies.where("created_at LIKE?", "%#{Time.zone.today}%").count
         @yesterday_comment=@user.commes.where("created_at LIKE?", "%#{Time.zone.yesterday}%").count+ @user.replies.where("created_at LIKE?", "%#{Time.zone.yesterday}%").count
     end
     
     def following
-        @title= "フォロー中の人"
+        @title ="フォロー中の人"
         @user=User.find(params[:id])
         @users=@user.following.page(params[:page]).per(20)
         render 'show_follow'
     end
     
     def followers
-        @titile= "フォロワー"
+        @title ="フォロワー"
         @user=User.find(params[:id])
         @users=@user.followers.page(params[:page]).per(20)
         render 'show_follow'
